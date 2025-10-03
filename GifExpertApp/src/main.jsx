fefb7c820431ce319225fd10637dfcb8fcdbf030
Colin-Hermes <colinhermes027@gmail.com>
2025-10-02 21:44:17 -0400

    Se creó el archivo GifExpertApp y el index.css para los estilos, aparte eliminé todo menos el main.jsx

--- GifExpertApp/src/main.jsx
+++ GifExpertApp/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './Index.css'
+import GifExpertApp from './components/GifExpertApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <GifExpertApp/>
   </StrictMode>,
 )
